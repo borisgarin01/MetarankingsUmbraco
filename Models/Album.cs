f92dd7161b2349c02baddba73be1e1ea70dc1584
Boris Garin <boris.garin01job@mail.ru>
2023-11-27 00:30:54 +0300

    Created Band,Album,Song
    Created Band,Album,Song, added image cropper to song entity, audio to song

---
+++ Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace MetarankingsUmbraco.Models
+{
+    [PublishedModel(contentTypeAlias: "album")]
+    public class Album : PublishedContentModel
+    {
+        public Album(IPublishedContent content, IPublishedValueFallback publishedValueFallback) : base(content, publishedValueFallback)
+        {
+        }
+
+        public string Title => this.Value<string>("title");
+    }
+}
